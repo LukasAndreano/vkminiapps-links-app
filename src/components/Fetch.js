0c8b5f95704c5ffe20e97301415967f72857c5b7
Lukas Andreano <business.nikitap@gmail.com>
2023-09-26 10:15:56 +0600

    fix: endpoint url

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,7 +1,7 @@
 export default async function fetch2(method, params) {
 	let fetch_params = params || "";
 	const data = await fetch(
-		"https://services.nbalin.dev/links/api.php?" +
+		"https://services.koka.team/links/api.php?" +
 			window.location.href.slice(window.location.href.indexOf("?") + 1),
 		{
 			method: "POST",
